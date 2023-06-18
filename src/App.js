28824c6ddf330cfe6c8202b03a77cabce14fbaa8
17swapna <rakesh.pothkanuri.96@gmail.com>
2023-06-18 17:11:04 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import ReactPaginate from 'react-paginate';
+import { CSVLink } from 'react-csv';
 
-function App() {
+const Dashboard = () => {
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("https://fakestoreapi.com/users");
+      setData(response.data);
+      setFilteredData(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+  const filterData = (searchTerm) => {
+    const filtered = data.filter((record) =>
+      Object.values(record).some((value) =>
+        value.toString().toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    );
+    setFilteredData(filtered);
+  };
+
+  const handlePageChange = ({ selected }) => {
+    setCurrentPage(selected);
+  };
+
+  const ITEMS_PER_PAGE = 50;
+  const offset = currentPage * ITEMS_PER_PAGE;
+  const pageCount = Math.ceil(filteredData.length / ITEMS_PER_PAGE);
+  const paginatedData = filteredData.slice(offset, offset + ITEMS_PER_PAGE);
+
+  const csvHeaders = [
+    { 
+      label: 'First Name', 
+      key: 'first_name'
+     },
+    { 
+      label: 'Last Name', 
+      key: 'last_name'
+     },
+    {
+       label: 'Email', 
+       key: 'email' 
+      },
+    { 
+      label: 'Phone Number',
+       key: 'phone_number' 
+      },
+ 
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='justify-center flex items-center'>
+      <h1>Admin Dashboard</h1>
+      <table>
+        <thead>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Email</th>
+          <th>Phone Number</th>
+      
+        </thead>
+        <tbody>
+
+          {
+          paginatedData.map((record, index) => (
+            <tr key={index}>
+              <td>{record.first_name}</td>
+              <td>{record.last_name}</td>
+              <td>{record.email}</td>
+              <td>{record.phone_number}</td>
+            
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <ReactPaginate
+        previousLabel={'Previous'}
+        nextLabel={'Next'}
+        pageCount={pageCount}
+        onPageChange={handlePageChange}
+        containerClassName={'pagination'}
+        activeClassName={'active'}
+      />
+      <CSVLink
+        data={filteredData}
+        headers={csvHeaders}
+        filename={'dashboard.csv'}
+      >
+        Download as CSV
+      </CSVLink>
     </div>
   );
-}
+};
 
-export default App;
+export default Dashboard;
